006958ca82b08f6e3cfbd3deb75abdff128fe6aa
Jose_Colli <jmicolli@gmail.com>
2020-11-18 01:24:37 -0600

    Adding Js to buttons showing modals, modifying modal colours uisng Bootstrap, JQuery and Sass

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('#btnShowReserveTableModal').click(function () {
+        $('#reserveModal').modal('show');
+    });
+
+});
+
+$(document).ready(function () {
+    $('#btnShowLoginModal').click(function () {
+        $('#loginModal').modal('show');
+    });
+
+});
